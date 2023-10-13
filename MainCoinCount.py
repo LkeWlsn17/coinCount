461f6ac9e032e06caf2235084d0042152b5acd9b
LkeWlsn17 <144439369+LkeWlsn17@users.noreply.github.com>
2023-10-13 13:51:00 +0100

    Add files via upload

--- MainCoinCount.py
+++ MainCoinCount.py
@@ -4,25 +4,34 @@ e = f.readlines()
 totalvalue = e[2].split('=')[1].strip()
 errors = e[0].split('=')[1].strip()
 amntbags = e[1].split('=')[1].strip()
+f.close()
 
-name = input("enter your name")
+print("""
+░█████╗░░█████╗░██╗███╗░░██╗  ░█████╗░░█████╗░██╗░░░██╗███╗░░██╗████████╗
+██╔══██╗██╔══██╗██║████╗░██║  ██╔══██╗██╔══██╗██║░░░██║████╗░██║╚══██╔══╝
+██║░░╚═╝██║░░██║██║██╔██╗██║  ██║░░╚═╝██║░░██║██║░░░██║██╔██╗██║░░░██║░░░
+██║░░██╗██║░░██║██║██║╚████║  ██║░░██╗██║░░██║██║░░░██║██║╚████║░░░██║░░░
+╚█████╔╝╚█████╔╝██║██║░╚███║  ╚█████╔╝╚█████╔╝╚██████╔╝██║░╚███║░░░██║░░░
+░╚════╝░░╚════╝░╚═╝╚═╝░░╚══╝  ░╚════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝░░░╚═╝░░░""")
+
+name = input("enter your name\n")
 isValidName = True
 for character in name:
     if character.isnumeric():
         isValidName = False
 
 if not isValidName:
-    input("enter a valid name")
+    input("enter a valid name\n")
 
-bagweight = float(input("Enter a bag weight"))
+bagweight = float(input("Enter a bag weight\n"))
 
 cointypes = 2, 1, 0.50 , 0.20, 0.10, 0.05, 0.02, 0.01
 
-cointype = float(input("enter the value of your coin"))
+cointype = float(input("enter the value of your coin\n"))
 
 remainder = bagweight%cointype
 if bagweight%cointype == 0:
-    print("Valid")
+    print("")
 else:
     print("Misweighed")
     print("you would need",remainder/cointype,"more coins to have a valid bagweight")
@@ -39,11 +48,24 @@ coinweights = {
 
 }
 
-totalbagvalue = coinweights[cointype]*bagweight
-totalbagvalue = float(totalbagvalue)
-print("the total value of this bag is",totalbagvalue,)
+mathbagvalue = float(bagweight/cointype)
+totalbagvalue = (coinweights[cointype]*mathbagvalue)
 
 totalvalue = float(totalvalue) + totalbagvalue
 totalvalue = str(totalvalue)
+totalbagvalue = str(totalbagvalue)
 
-f.write(totalvalue)[2]
+volunteers = []
+f = open("values.txt")
+e = f.readlines()
+volunteers.append(e[3].split('=')[1].strip())
+volunteers.append(name)
+                
+f.close()
+
+
+print("the total value of this bag is",totalbagvalue,)
+filevalue = f"errors = {errors}\namntbags = {amntbags}\ntotalbagvalue = {totalvalue}\nvolunteers={volunteers}"
+
+f = open("values.txt",'r+')
+f.write(filevalue)
